1030286a691bb11a885d861d731495de9532c998
Gustavo Coronel <gcoronelc@gmail.com>
2017-09-05 12:38:54 -0500

    Sem 04

---
+++ Sem04/Calculadora/src/pe/egcc/calculadora/service/CalculaService.java
@@ -0,0 +1,51 @@
+package pe.egcc.calculadora.service;
+
+public class CalculaService {
+  
+  
+  public int sumar(int n1, int n2){
+    // Variable
+    int suma;
+    // Proceso
+    suma = n1 + n2;
+    // Retorno
+    return suma;
+  }
+  
+  public int restar(int n1, int n2){
+    // Variable
+    int resta;
+    // Proceso
+    resta = n1 - n2;
+    // Retorno
+    return resta;
+  }
+  
+  public int multiplicar(int n1, int n2){
+    // Variable
+    int prod;
+    // Proceso
+    prod = n1 * n2;
+    // Retorno
+    return prod;
+  }
+  
+  public int dividir(int n1, int n2){
+    // Variable
+    int cociente;
+    // Proceso
+    cociente = n1 / n2;
+    // Retorno
+    return cociente;
+  }
+  
+  public int resto(int n1, int n2){
+    // Variable
+    int cociente;
+    // Proceso
+    cociente = n1 / n2;
+    // Retorno
+    return cociente;
+  }
+  
+}
